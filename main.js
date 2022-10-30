8cd987823d163b55275a606f3f9e43dad6b5c018
WilliamKing723 <william72314@gmail.com>
2022-10-31 01:27:00 +0800

    remove debug

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
-  const win = new BrowserWindow({
+const win = new BrowserWindow({
     width: 800,
     height: 600
-  })
+})
 
-  win.openDevTools();
+// win.openDevTools();
 
-  win.loadFile('./html/index.html')
+win.loadFile('./html/index.html')
 }
 
 app.whenReady().then(() => {
-  createWindow()
+createWindow()
 })
