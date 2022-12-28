8b944f316204e3305549c01f3e8011c71e51e72b
lusterofgem <lusterofgem@gmail.com>
2022-12-29 01:25:25 +0800

    change title

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-const { app, BrowserWindow } = require('electron')
-const createWindow = () => {
+const { app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
 	const win = new BrowserWindow({
 		width: 800,
-		height: 600
+		height: 600,
 	});
 
 	// win.openDevTools();
 
 	win.loadFile('./html/index.html');
-}
-
-app.whenReady().then(() => {
-	createWindow();
-})
+});
